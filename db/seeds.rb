2a6f7d47b456e82d89691d5d64ccec05ecd46f04
Lee Knowlton <aeriath@gmail.com>
2018-04-29 21:52:37 +1200

    added teams and world cup seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+world_cup_teams = ['Russia', 'Uruguay', 'Egypt', 'Saudi Arabia',
+    'Portugal', 'Spain', 'Iran', 'Morocco',
+    'France', 'Peru', 'Denmark', 'Australia',
+    'Argentina', 'Croatia', 'Iceland', 'Nigeria',
+    'Brazil', 'Switzerland', 'Costa Rica', 'Serbia',
+    'Germany', 'Mexico', 'Swedan', 'South Korea',
+    'Belgium', 'England', 'Tunisia', 'Panama',
+    'Poland', 'Colombia', 'Senegal', 'Japan']
+
+puts 'IMPORTING WORLD CUP TEAMS'
+world_cup_teams.each do |t|
+    new_team = Team.find_or_create_by(name: t)
+    puts 'IMPORTED: ' << new_team.name
+end
